2a3160d8a561635d8ec480afd543c8569af9fa81
A-Tanner <atanner5@ewu.edu>
2022-01-23 15:46:58 -0800

    Implement ConfigureFileLogger

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,10 +1,28 @@
 ﻿namespace Logger
 {
+
     public class LogFactory
     {
-        public BaseLogger CreateLogger(string className)
+        //CreateLogger should probably have static functionality. Need to research.
+        public BaseLogger? CreateLogger(string className)
         {
+            if (filepath is null)
+            {
+                return null;
+            }
+
+            //Possibly need to rewrite FileLogger to delegate construction to BaseLogger.
+            //This would allow BaseLogger to take a className and FileLogger to take a filepath
+
             return new BaseLogger(className);
         }
+
+        public void ConfigureFileLogger(string fp)
+        {
+            if (fp is not null)
+            filepath = fp;
+        }
+
+        private string? filepath = null;
     }
 }
