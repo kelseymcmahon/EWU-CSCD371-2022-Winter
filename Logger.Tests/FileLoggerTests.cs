443b3fc2b7dbcb0d5d276f20b4b7325cc09009a4
Kelsey McMahon <40438334+kelseymcmahon@users.noreply.github.com>
2022-01-24 12:07:42 -0800

    Comments

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -5,18 +5,28 @@ namespace Logger.Tests
     [TestClass]
     public class FileLoggerTests
     {
+
+        string filePath = "test.txt";
+        string message = "This is an standard message";
+
         [TestMethod]
 
         public void Log_CreateMessageLogString()
         {
-            string fileName = "test.txt";
-            BaseLogger? log = LogFactory.CreateLogger(fileName);
-            string message = "This is an error message";
+            //create the log file
+            BaseLogger? log = LogFactory.CreateLogger(filePath);
 
-            log.Log(LogLevel.Error, message);
+            log?.Log(LogLevel.Error, message);
 
-            Assert.IsInstanceOfType(log, typeof(string));
+            //need to check the line written on the file from calling the Log method
+            //Assert.IsInstanceOfType(log, typeof(string));
+
+            //delete log file after testing
+            File.Delete(filePath);
                 
         }
+
+        //check date time in log method
+        //
     }
 }
