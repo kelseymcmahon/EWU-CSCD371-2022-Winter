886c0dcb1c2db76ae957b128888b88edd2122f6a
A-Tanner <atanner5@ewu.edu>
2022-01-27 11:45:53 -0800

    Added ConsoleLogger.cs

---
+++ Logger/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    internal class ConsoleLogger : BaseLogger
+    {
+        public override void Log(LogLevel logLevel, string message)
+        {
+            string log = DateTime.Now.ToString() + " | " + this.Name + " | " + logLevel.ToString() + " | " + message + System.Environment.NewLine;
+            Console.Write(log);
+        }
+
+        
+    }
+}
