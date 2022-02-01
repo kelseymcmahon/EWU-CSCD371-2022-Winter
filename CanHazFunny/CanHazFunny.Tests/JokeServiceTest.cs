da4a92802689b5a491e34ae88f4a09b219bff84e
whitneybolar <wbolar@eagles.ewu.edu>
2022-02-01 12:32:08 -0800

    Added Joke Service Tests

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny.Tests
+{
+    [TestClass]
+    public class JokeServiceTest
+    {
+        [TestMethod]
+        public void GetJoke_String_Data()
+        {
+            JokeService service = new JokeService();
+            
+            Assert.IsInstanceOfType(service.GetJoke(), typeof(string));
+        }
+
+       [TestMethod]
+        public void GetJoke_Null_CorrectData()
+        {
+            JokeService service = new JokeService();
+
+            Assert.IsNotNull(service.GetJoke());
+        }
+    }
+}
