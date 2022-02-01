13bf94a87c9489a4fe6860fd41bd81a103491a5a
Kelsey McMahon <kelsey.mcmahon@hotmail.com>
2022-02-01 12:37:44 -0800

    Added some tests.

---
+++ CanHazFunny/CanHazFunny.Tests/WriteJokeServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace CanHazFunny.Tests
+{
+    [TestClass]
+    public class WriteJokeServiceTest
+    {
+        string joke = "Did you hear about the claustrophobic astronaut? He just wanted a bit more space.";
+        WriteJokeService writeJokeService;
+        StringWriter stringWriter = new();
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            writeJokeService = new();
+            Console.SetOut(stringWriter);
+        }
+
+        [TestMethod]
+        public void WriteJokeToConsole_GetsJokeString_NotNull()
+        {
+            writeJokeService.WriteJokeToConsole(joke);
+            Assert.IsNotNull(stringWriter.ToString());        
+        }
+
+        [TestMethod]
+        public void WriteJokeToConsole_GetsJokeString_()
+        {
+            writeJokeService.WriteJokeToConsole(joke);
+            Assert.IsInstanceOfType(writeJokeService, IWriteJoke);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+    }
+}
