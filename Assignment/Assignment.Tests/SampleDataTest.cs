b970119552d7076968b507170ee41c82875d9bb0
Kelsey McMahon <kelsey.mcmahon@hotmail.com>
2022-02-15 20:52:56 -0800

    Setup.

---
+++ Assignment/Assignment.Tests/SampleDataTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment.Tests;
+
+[TestClass]
+public class SampleDataTest
+{
+    [TestMethod]
+    public void CvsRows_GetData_Success()
+    {
+        SampleData sample = new();
+
+        sample.CsvRows.GetEnumerator().ToString();
+    }
+}
+
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -6,7 +6,14 @@ namespace Assignment
     public class SampleData : ISampleData
     {
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows
+        {
+            get 
+            { 
+                string[] lines = System.IO.File.ReadAllLines(@"People.csv");
+                return lines; 
+            }
+        }
 
         // 2.
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
