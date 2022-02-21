8a1458c3dfad7a3484483a0279578407befe7fe5
Kelsey McMahon <40438334+kelseymcmahon@users.noreply.github.com>
2022-02-21 14:58:33 -0800

    Added Node class and NodeTest class.

---
+++ Assignment/Assignment.Tests/NodeTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class NodeTest
+    {
+        [TestMethod]
+        public void Node_NodeIsAdded_Success()
+        {
+            Node<string> node = new("Kelsey");
+
+            Assert.AreEqual<string>(node.Value, "Kelsey");
+            Assert.AreEqual<Node<string>>(node, node.Next);
+        }
+
+        [TestMethod]
+        public void Append_NodeLinksAreCircular_Success()
+        {
+            Node<string> node1 = new("Kelsey");
+            Node<string> node2 = new("Whitney");
+            Node<string> node3 = new("Leona");
+
+            node1.Append(node2);
+            node2.Append(node3);
+
+            Assert.AreEqual<Node<string>>(node1.Next, node2);
+            Assert.AreEqual<Node<string>>(node2.Next, node3);
+            Assert.AreEqual<Node<string>>(node3.Next, node1);
+        }
+
+    }
+}
---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public void Append(Node<T> NewNode)
+        {
+            Node<T> temp = this.Next;
+            Next = NewNode;
+            NewNode.Next = temp;
+        }
+    }
+}
