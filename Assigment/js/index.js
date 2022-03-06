94fbec402d5d85eb43b399cab675e46e6278fd1f
Kelsey McMahon <kelsey.mcmahon@hotmail.com>
2022-03-05 22:59:05 -0800

    Added axios joke call.

--- Assigment/js/index.js
+++ Assigment/js/index.js
@@ -1,3 +1,28 @@
 function showMenu() {
     document.querySelector(".dropdown-items").classList.toggle("show");
 }
+
+
+axios({
+    method: 'get',
+    url: 'https://v2.jokeapi.dev/joke/Programming'
+})
+    .then(function (response) {
+        console.log(response);
+        console.log(response.data.setup);
+
+        let jokeSetup = document.querySelector(".jokeSetup");
+        let jokePunchline = document.querySelector(".jokePunchline");
+        jokeSetup.innerHTML = "<p>" + response.data.setup + "</p>";
+        setTimeout(() => { jokePunchline.innerHTML = "<p>" + response.data.delivery + "</p>"; }, 4000);
+    })
+    //.catch(function (error) {
+    //    console.log(error);
+    //    axios.get;
+    //});
+
+function getNewJoke() {
+
+    axios.get();
+    console.log("worked");
+}
